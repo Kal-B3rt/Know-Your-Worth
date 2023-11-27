41e24a2f89b63f396af1e5bd8df374dd47ccb667
kal <kennyandmeghan@gmail.com>
2023-11-27 14:16:26 -0500

    Initial Commit

---
+++ Know Your Worth/Know_Your_WorthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Know_Your_WorthApp.swift
+//  Know Your Worth
+//
+//  Created by Kenny Albert on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Know_Your_WorthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
